8b8f44598d2a7cebc6e19122e1dcc70b23323b1c
LachlanNewman <lachnewman007@gmail.com>
2018-12-22 17:03:31 +1100

    Lazy Load Solutions

--- client/src/components/AlternateSectionList.js
+++ client/src/components/AlternateSectionList.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import LazyLoad from 'react-lazy-load';
 
 import AlternateFlipCard from './AlternateFlipCard';
+
 
 class AlternateSectionList extends React.Component {
 
 	renderList() {
-		const colors = ['grey-dark','blue-dark','blue-medium']
-		console.log(colors)
-		return this.props.items.map((item,i) => {
+		const colors = ['grey-dark', 'blue-dark', 'blue-medium']
+		return this.props.items.map((item, i) => {
 			const color = colors[i % 3];
-			return <AlternateFlipCard item={item} color={color}/>
+			return (
+				<LazyLoad>
+					<AlternateFlipCard item={item} color={color}/>
+				</LazyLoad>
+			)
 		})
 	}
 

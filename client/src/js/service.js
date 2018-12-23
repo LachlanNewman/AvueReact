5225d56d188a947489829b074feec73e63709725
LachlanNewman <lachnewman007@gmail.com>
2018-12-23 18:45:20 +1100

    Sevice Images Medium

--- client/src/js/service.js
+++ client/src/js/service.js
@@ -9,14 +9,14 @@ const service = {
 			title: 'SECURITY',
 			subTitle: 'Protecting your investment and information through effective security full service operations',
 			text: 'Protecting your investment and information through effective security full service operations;  secure, defendable, contained, managed and  predictive. ',
-			img: './assets/images/Security.jpg',
+			img: './assets/images/Security_4382303_m-2015.jpg',
 		},
 		{
 			id: 1,
 			title: 'CONTINUED SUPPORT AND SERVICE',
 			subTitle: 'Ensuring ongoing benefit realisation through ITIL based managed services and delivery of sustained TCO for multiple environments',
 			text: 'Ensuring ongoing benefit realisation through ITIL based managed services and delivery of sustained TCO for multiple environments.  Addressing applications and system capacity for access to corporate information and integrated solutions.',
-			img: './assets/images/Support.jpg',
+			img: './assets/images/Support_25275685_m-2015.jpg',
 
 		}
 	]

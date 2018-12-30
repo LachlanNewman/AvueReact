c0fe1da6e83a7098eaa20d76941c21a1c43af9aa
LachlanNewman <lachnewman007@gmail.com>
2018-12-30 14:30:17 +1100

    Offset of top for Desktop

--- public/assets/js/navbar.js
+++ public/assets/js/navbar.js
@@ -1,10 +1,19 @@
 $(function () {
+
+	const desktop = 768;
+	const screenWidth = $(window).width();
+
 	$('.nav-menu__link').click(function () {
 		const id = '#' + $(this).data('scroll');
+
+		let offset = 60;
+		if(screenWidth < desktop){
+			offset = 270;
+		}
 		$('#nav').removeClass('show-menu');
 		$('#main').removeClass('show-menu');
 		$("html, body").animate({
-			scrollTop: $(id).offset().top - 270
+			scrollTop: $(id).offset().top - offset
 		}, 2000);
 	})
 	
